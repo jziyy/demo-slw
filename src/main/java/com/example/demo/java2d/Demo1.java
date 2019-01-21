999fab34743fcef21c01f02f28797cc76d1af749
jziyy <13244572861@163.com>
2019-01-21 15:13:47 +0800

    划线

--- src/main/java/com/example/demo/java2d/Demo1.java
+++ src/main/java/com/example/demo/java2d/Demo1.java
@@ -1,7 +1,25 @@
 package com.example.demo.java2d;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+
 public class Demo1 {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		File file = new File("C:\\Users\\sjziy\\Desktop\\新建文件夹\\无标题.png");
+		BufferedImage image = ImageIO.read(file);
+		Graphics2D graphics2D = image.createGraphics();
+		//设置线段颜色
+		graphics2D.setColor(Color.red);
+		//设置线段宽度
+		graphics2D.setStroke(new BasicStroke(20f));
+		//划线
+		graphics2D.drawLine(0,0,1111111,1111111);
+		//保存
+		ImageIO.write(image,"PNG",file);
+
 	}
 }
